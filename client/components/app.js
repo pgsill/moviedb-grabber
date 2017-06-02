a1a4c26677212542ed81d8e7216b1cb41c35c97e
Erthyl Tann <pedroion@gmail.com>
2017-06-02 20:50:19 -0300

    Removed LoginPage and App components (unused)

--- client/components/app.js
+++ client/components/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Match } from 'react-router-dom';
 
-import Home from './home';
 import Menu from './menu';
 
 import MovieDetail from './movie-detail';
